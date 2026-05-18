e897ffabd6b398f329aba47dc24b949327708d44
Andy W... <andy.wuehler@gmail.com>
2026-05-17 21:12:59 -0700

    purge localhost credentials

--- 0.2/cqr_env.py
+++ 0.2/cqr_env.py
@@ -4,5 +4,5 @@ PUMP_FEATURE = ["3.5 GPM", "45 PSI"]            # Seed values pump
 TANK_FEATURE = ["5.0 GAL", "12 INCH"]           # Seed values tank
 TIME_CHECKED = ["17.19", "43"]                  # Intervals (seconds)
 BOOL_OUTPUTS = [True]                           # Extra Stdout
-PDUS_SMARTLY = ["DWL", "192.168.156.109", "admin", "admin", "7"]  # PDU
+PDUS_SMARTLY = ["DWL", "192.168.XXX.YYY", "userX", "passY", "7"]  # PDU
 # ----------------------------------------------------------------------
